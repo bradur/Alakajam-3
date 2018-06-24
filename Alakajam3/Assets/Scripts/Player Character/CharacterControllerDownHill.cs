7e1843141af7ce00a3af99b25c69f0b06d38724d
Juutis <jutkula@gmail.com>
2018-06-24 14:22:27 +0300

    Added snowball prefab

--- Alakajam3/Assets/Scripts/Player Character/CharacterControllerDownHill.cs
+++ Alakajam3/Assets/Scripts/Player Character/CharacterControllerDownHill.cs
@@ -12,11 +12,14 @@ public class CharacterControllerDownHill : MonoBehaviour
 
     float horizontalAxis, verticalAxis;
 
+    private SphereCollider ballCollider;
+
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.maxAngularVelocity = 100f;
+        ballCollider = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
@@ -33,8 +36,8 @@ public class CharacterControllerDownHill : MonoBehaviour
         Vector3 right = Camera.main.transform.right;
         fwd = new Vector3(fwd.x, 0, fwd.z);
         right = new Vector3(right.x, 0, right.z);
-        rb.AddTorque(right * verticalAxis * turnSpeed, ForceMode.Force);
-        rb.AddTorque(-fwd * horizontalAxis * turnSpeed, ForceMode.Force);
+        rb.AddTorque(right * verticalAxis * turnSpeed * ballCollider.radius, ForceMode.Force);
+        rb.AddTorque(-fwd * horizontalAxis * turnSpeed * ballCollider.radius, ForceMode.Force);
         //rb.AddTorque(0, 0, horizontalAxis * turnSpeed, ForceMode.Force);
     }
 
